4531b21be0c5d21f41bb292e33d8ee81694ecd1c
Your Name <you@example@example.com>
2026-09-15 20:32:04 +0100

    Dynamic Product Detail Page requirements

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
@@ -12,7 +12,10 @@ async function addToCartHandler(e) {
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+const productId = getParam("product");
+console.log(dataSource.findProductById(productId));
+// add to cart button event handler
+
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
